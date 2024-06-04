3fb30931df87ded15baaf570a6e386add6aba759
blue3212 <luckywill339@gmail.com>
2024-06-03 22:32:44 -0400

    Client side something

---
+++ Content.Client/_NF/Marketplace/UI/MarketplaceUiFragment.xaml.cs
@@ -0,0 +1,13 @@
+using Content.Client.UserInterface.Fragments;
+using Content.Shared._NF.BountyContracts;
+using JetBrains.Annotations;
+using Robust.Client.GameObjects;
+using Robust.Client.UserInterface;
+
+namespace Content.Client._NF.Marketplace.UI;
+
+[UsedImplicitly]
+public sealed partial class MarketplaceUiFragment
+{
+
+}
